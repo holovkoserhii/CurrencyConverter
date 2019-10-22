636f1ff7bf1259a464920876b7b9d13ea1c40dc7
Serhii Holovko <holovko.s@gmail.com>
2019-10-22 22:35:20 +0300

    add conversion rate view

---
+++ app/components/Text/styles.js
@@ -0,0 +1,11 @@
+import EStyleSheet from "react-native-extended-stylesheet";
+
+const styles = EStyleSheet.create({
+  smallText: {
+    color: "$mainHeaderTextColor",
+    fontSize: 12,
+    textAlign: "center",
+  },
+});
+
+export default styles;
--- app/screens/Home.js
+++ app/screens/Home.js
@@ -5,8 +5,28 @@ import { Container } from "../components/Container";
 import { Logo } from "../components/Logo";
 import { InputWithButton } from "../components/TextInput";
 import { ClearButton } from "../components/Buttons";
+import { LastConverted } from "../components/Text";
 
 export default () => {
+  const TEMP_BASE_CURRENCY = "USD";
+  const TEMP_QOUTE_CURRENCY = "GBP";
+  const TEMP_BASE_PRICE = "100";
+  const TEMP_QOUTE_PRICE = "79.74";
+  const TEMP_CONVERSION_RATE = 0.7974;
+  const TEMP_CONVERSION_DATE = new Date();
+
+  const handleChangeText = () => {
+    console.log("text changed");
+  };
+
+  const handlePressBaseCurrency = () => {
+    console.log("press base currency");
+  };
+
+  const handlePressQuoteCurrency = () => {
+    console.log("press quote currency");
+  };
+
   const swapCurrency = () => {
     console.log("press swap");
   };
@@ -16,23 +36,23 @@ export default () => {
       <StatusBar translucent={false} barStyle="light-content" />
       <Logo />
       <InputWithButton
-        buttonText="blah1"
-        onPress={() => {
-          console.log("pressed 1");
-        }}
-        defaultValue="99"
+        buttonText={TEMP_BASE_CURRENCY}
+        onPress={handlePressBaseCurrency}
+        defaultValue={TEMP_BASE_PRICE}
         keyboardType="numeric"
-        onChangeText={() => {
-          console.log("text changed");
-        }}
+        onChangeText={handleChangeText}
       />
       <InputWithButton
-        buttonText="blah2"
-        onPress={() => {
-          console.log("pressed 2");
-        }}
+        buttonText={TEMP_QOUTE_CURRENCY}
+        onPress={handlePressQuoteCurrency}
         editable={false}
-        value="77"
+        value={TEMP_QOUTE_PRICE}
+      />
+      <LastConverted
+        base={TEMP_BASE_CURRENCY}
+        quote={TEMP_QOUTE_CURRENCY}
+        date={TEMP_CONVERSION_DATE}
+        conversionRate={TEMP_CONVERSION_RATE}
       />
       <ClearButton text="Reverse currencies" onPress={swapCurrency} />
     </Container>
